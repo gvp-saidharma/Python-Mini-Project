1dcc2e71d4aaa7440bc363da5b7aa82dbcda934d
Sai Dharma Reddy <saidharmareddy@gmail.com>
2021-10-04 16:30:56 +0530

    Add files via upload

--- ChatBot.py
+++ ChatBot.py
@@ -1,24 +1,92 @@
-from tkinter import *
-def send():
-    send = "me:"+ e.get()
-    text.insert(END,"\n" + send)
-    if(e.get()=='hi'):
-        text.insert(END, "\n" + "Bot: hi")
-    elif(e.get()=='hello'):
-        text.insert(END, "\n" + "Bot: hello")
-    elif (e.get() == 'how are you?'):
-        text.insert(END, "\n" + "Bot: i'm fine and you?")
-    elif (e.get() == "i'm fine too"):
-        text.insert(END, "\n" + "Bot: nice to hear that")
-    else:
-        text.insert(END, "\n" + "Bot: Sorry I didnt get it.")
 
-root = Tk()
-text = Text(root,bg='light blue')
-text.grid(row=0,column=0,columnspan=2)
-e = Entry(root,width=80)
-send = Button(root,text='Send',bg='blue',width=20,command=send).grid(row=1,column=1)
-e.grid(row=1,column=0)
-# root = Tk()
-root.title('Mini Project Sample Chat Bot')
-root.mainloop()
+#Meet Robo: your friend
+
+#import necessary libraries
+import io
+import random
+import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True) # for downloading packages
+
+# uncomment the following only the first time
+#nltk.download('punkt') # first-time use only
+#nltk.download('wordnet') # first-time use only
+
+
+#Reading in the corpus
+with open('chatbot.txt','r', encoding='utf8', errors ='ignore') as fin:
+    raw = fin.read().lower()
+
+#TOkenisation
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+# Preprocessing
+lemmer = WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# Keyword Matching
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+
+flag=True
+print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("ROBO: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("ROBO: "+greeting(user_response))
+            else:
+                print("ROBO: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")    
+        
+        
+
